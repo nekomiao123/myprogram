3d7c7ecfb3e6b3d8d05288c6c8c5de3dfed1430d
neko <447140443a@gmail.com>
2018-11-13 10:08:30 +0800

    little

--- data/littlexi.cpp
+++ data/littlexi.cpp
@@ -4,16 +4,15 @@ int main(){
     int N=3;
 	//scanf("%d",&N);
     int all = 0;
-    /*for(int i=0;i<N;i++)
+    for(int i=0;i<N;i++)
     {
         int j= 0;
         j = 9*pow(10,i);
         all += 9*pow(10,i);
         printf("1...%d\n",j);
         printf("%d\n",all);
-    }*/
+    }
 
-    int a = pow(10,2);
    
     printf("9^2=%d\n",a);
     //printf("%d",all);
